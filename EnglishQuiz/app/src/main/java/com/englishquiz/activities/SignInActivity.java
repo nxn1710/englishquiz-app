19fdcba648eb177eb8713bb015218a27b5e42d84
TaBiLo <tblong2105@gmail.com>
2021-10-07 22:55:05 +0700

    [EnglishQuiz][Coding] Add screen SignIn

--- EnglishQuiz/app/src/main/java/com/englishquiz/activities/SignInActivity.java
+++ EnglishQuiz/app/src/main/java/com/englishquiz/activities/SignInActivity.java
@@ -6,22 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.englishquiz.MainActivity;
 import com.englishquiz.R;
 
 public class SignInActivity extends AppCompatActivity {
     Button btn;
+    TextView txtRegisterNow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        btn = findViewById(R.id.button2);
+        btn = findViewById(R.id.btnSignIn);
+        txtRegisterNow = findViewById(R.id.txtRegisterNow);
         Intent i = new Intent(this, MainActivity.class);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(i);
+            }
+        });
+
+        Intent iRegisterNow = new Intent(this, SignUpActivity.class);
+        txtRegisterNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(iRegisterNow);
             }
         });
     }
