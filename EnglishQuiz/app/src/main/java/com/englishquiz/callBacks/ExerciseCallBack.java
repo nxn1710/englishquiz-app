a9cf451ca4128ce1492b34b66e6a2de87878e604
thuan895 <nguyenvanthuan895@gmail.com>
2021-11-02 01:52:54 +0700

    [Thuan895] merge Develop

--- EnglishQuiz/app/src/main/java/com/englishquiz/callBacks/ExerciseCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.englishquiz.callBacks;
-
-import com.englishquiz.model.Exercise;
-
-import java.util.HashMap;
-
-public interface ExerciseCallBack {
-    public void onCallbackExercise(HashMap<String, Exercise> value);
-}
