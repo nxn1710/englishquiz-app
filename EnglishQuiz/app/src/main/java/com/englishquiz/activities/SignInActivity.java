2302278c10a93dc03e560548e54bc8c95d31fe21
TaBiLo <tblong2105@gmail.com>
2021-11-03 22:41:05 +0700

    Handle authentication (login, register, logout) and edit screen (home, signin, signup )

--- EnglishQuiz/app/src/main/java/com/englishquiz/activities/SignInActivity.java
+++ EnglishQuiz/app/src/main/java/com/englishquiz/activities/SignInActivity.java
@@ -1,30 +1,51 @@
 package com.englishquiz.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.englishquiz.MainActivity;
 import com.englishquiz.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
     Button btn;
     TextView txtRegisterNow;
+    EditText edtEmail, edtPassword, edtRePassword;
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        // taking FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
         btn = findViewById(R.id.btnSignIn);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtRePassword = findViewById(R.id.edtRePassword);
+
         txtRegisterNow = findViewById(R.id.txtRegisterNow);
         Intent i = new Intent(this, MainActivity.class);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(i);
+                loginUserAccount();
             }
         });
 
@@ -35,5 +56,53 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(iRegisterNow);
             }
         });
+    }
+
+
+    private void loginUserAccount()
+    {
+        String email, password;
+        email = edtEmail.getText().toString();
+        password = edtPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter email!!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                    "Please enter password!!",
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // signin existing user
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(
+                                    @NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful()) {
+                                    // if sign-in is successful
+                                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(i);
+                                }
+
+                                else {
+                                    // sign-in failed
+                                    Toast.makeText(getApplicationContext(),
+                                            "Login failed!!",
+                                            Toast.LENGTH_LONG)
+                                            .show();
+                                }
+                            }
+                        });
     }
 }
